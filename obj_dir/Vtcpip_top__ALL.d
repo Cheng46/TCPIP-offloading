d191715079e3b6c747c3ab4c412d6fd2a33427f3
cheng46 <10430224ghi@gmail.com>
2025-04-17 23:05:29 +0800

    initial commit

--- obj_dir/Vtcpip_top__ALL.d
+++
@@ -1,7 +0,0 @@
-Vtcpip_top__ALL.o: Vtcpip_top__ALL.cpp Vtcpip_top.cpp Vtcpip_top.h \
- /usr/share/verilator/include/verilated_heavy.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vtcpip_top__Syms.h \
- Vtcpip_top__Trace.cpp /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated_trace.h Vtcpip_top__Slow.cpp \
- Vtcpip_top__Syms.cpp Vtcpip_top__Trace__Slow.cpp
